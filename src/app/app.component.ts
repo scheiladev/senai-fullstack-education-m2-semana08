e8f9401f968a9971be0bca0fff079c1eb9ac5fdb
Scheila Stihler <sstihler@gmail.com>
2024-07-11 20:53:16 -0300

    Resolução do exercício 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,22 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, CommonModule, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  showHeader = true;
+  isAppPage = true;
 
   constructor(private router: Router) {
     this.router.events.subscribe((retorno) => {
       if (retorno instanceof NavigationEnd) {
-        this.showHeader = !this.router.url.includes('login');
+        this.isAppPage = !this.router.url.includes('login');
       }
     });
   }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
-
-}
+export class SidebarComponent {}
